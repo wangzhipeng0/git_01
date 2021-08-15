b90d6e4201fb3c1ae94e39704f0527e1cc93a5c4
wangzhipeng <wangzhipeng@atguigu.com>
2021-08-15 16:13:54 +0800

    3

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,8 +1,8 @@
-
 public class test {
     public static void main(String[] args) {
         System.out.println("v1.0");
         System.out.printf("2.0");
+        System.out.printf("3.0");
     }
 }
 
