383c16b417ab99568c7fbaaa28a5456ee5c72624
wangzhipeng <wangzhipeng@atguigu.com>
2021-08-15 14:52:17 +0800

    2.0

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,13 +1,8 @@
-/**
- * 功能描述
- *
- * @author 王志鹏
- * @date 2021/08/15  14:49
- */
 
 public class test {
     public static void main(String[] args) {
         System.out.println("v1.0");
+        System.out.printf("2.0");
     }
 }
 
